b6addf0a95a30072793a5a9f5b0973174cebe4dc
chax2 <74134231+chax2@users.noreply.github.com>
2020-11-18 19:24:16 +0700

    Add files via upload

---
+++ feature.js
@@ -0,0 +1,156 @@
+var num0 = 0 ,num1 = 0,num2 = 0,num3 = 0, num4 = 0,num5=0,num6=0,num7=0,num8=0,num9=0;
+function hide0() {
+    if (num0 == 0) {
+        document.getElementById("chevoron0").className = "fa fa-chevron-down";
+
+        document.getElementById("PipeInfor").style.display = "block";
+        num0++;
+        
+        
+    }
+     else {
+        document.getElementById("chevoron0").className = "fa fa-chevron-up";
+        document.getElementById("PipeInfor").style.display = "none";
+        num0=0;
+
+    }
+}
+function hide1() {
+    if (num1 == 0) {
+        document.getElementById("chevoron1").className = "fa fa-chevron-down";
+        document.getElementById("WorldInfor").style.display = "block";
+        num1++;
+        
+        
+    }
+     else {
+        document.getElementById("chevoron1").className = "fa fa-chevron-up";
+        document.getElementById("WorldInfor").style.display = "none";
+        
+        num1=0;
+
+    }
+
+}
+function hide2() {
+    if (num2 == 0) {
+        document.getElementById("chevron2").className = "fa fa-chevron-down";
+        document.getElementById("AnimaInfor").style.display = "block";
+        num2++;
+        
+        
+    }
+     else {
+        document.getElementById("chevron2").className = "fa fa-chevron-up";
+        document.getElementById("AnimaInfor").style.display = "none";
+        num2=0;
+
+    }
+}
+function hide3() {
+    if (num3 == 0) {
+        document.getElementById("chevron3").className = "fa fa-chevron-down";
+        document.getElementById("RenInfor").style.display = "block";
+        num3++;
+        
+        
+    }
+     else {
+        document.getElementById("chevron3").className = "fa fa-chevron-up";
+        document.getElementById("RenInfor").style.display = "none";
+        num3=0;
+
+    }
+}
+function hide4() {
+    if (num4 == 0) {
+        document.getElementById("chevron4").className = "fa fa-chevron-down";
+        document.getElementById("SimInfor").style.display = "block";
+        num4++;
+        
+        
+    }
+     else {
+        document.getElementById("chevron4").className = "fa fa-chevron-up";
+        document.getElementById("SimInfor").style.display = "none";
+        num4=0;
+
+    }
+}
+function hide5() {
+    if (num5 == 0) {
+        document.getElementById("chevron5").className = "fa fa-chevron-down";
+        document.getElementById("InterInfor").style.display = "block";
+        num5++;
+        
+        
+    }
+    else {
+        document.getElementById("chevron5").className = "fa fa-chevron-up";
+        document.getElementById("InterInfor").style.display = "none";
+        num5=0;
+
+    }
+}
+function hide6() {
+    if (num6 == 0) {
+        document.getElementById("chevron6").className = "fa fa-chevron-down";
+        document.getElementById("IntegInfor").style.display = "block";
+        num6++;
+        
+        
+    }
+    else {
+        document.getElementById("chevron6").className = "fa fa-chevron-up";
+        document.getElementById("IntegInfor").style.display = "none";
+        num6=0;
+
+    }
+}
+function hide7() {
+    if (num7 == 0) {
+        document.getElementById("chevron7").className = "fa fa-chevron-down";
+        document.getElementById("PlateInfor").style.display = "block";
+        num7++;
+        
+        
+    }
+    else {
+        document.getElementById("chevron7").className = "fa fa-chevron-up";
+        document.getElementById("PlateInfor").style.display = "none";
+        num7=0;
+
+    }
+}
+function hide8() {
+    if (num8 == 0) {
+        document.getElementById("chevron8").className = "fa fa-chevron-down";
+        document.getElementById("ConInfor").style.display = "block";
+        num8++;
+        
+        
+    }
+    else {
+        document.getElementById("chevron8").className = "fa fa-chevron-up";
+        document.getElementById("ConInfor").style.display = "none";
+        num8=0;
+
+    }
+}
+function hide9() {
+    if (num8 == 0) {
+        document.getElementById("chevron9").className = "fa fa-chevron-down";
+        document.getElementById("DevInfor").style.display = "block";
+        num8++;
+        
+        
+    }
+    else {
+        document.getElementById("chevron9").className = "fa fa-chevron-up";
+        document.getElementById("DevInfor").style.display = "none";
+        num8=0;
+
+    }
+}
+
+
